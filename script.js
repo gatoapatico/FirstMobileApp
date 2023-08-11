a7b72d1ccbf76c6e67073f9beedf04c8ca880123
ALVARO FERNANDEZ <aferzu@gmail.com>
2023-08-11 00:20:08 -0500

    Skeleton app

---
+++ script.js
@@ -0,0 +1,6 @@
+const inputField = document.getElementById("input-field");
+const addButton = document.getElementById("add-button");
+
+addButton.addEventListener("click", function(){
+    console.log(inputField.value);
+});
