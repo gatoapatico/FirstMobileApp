aeb322a65758b2c7f38769ab1b2e2104adb8e453
ALVARO FERNANDEZ <aferzu@gmail.com>
2023-08-14 00:26:19 -0500

    Add to cart last commit

--- script.js
+++ script.js
@@ -1,6 +1,62 @@
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.1.0/firebase-app.js';
+import { getDatabase, ref, push, onValue, remove } from 'https://www.gstatic.com/firebasejs/10.1.0/firebase-database.js';
+
+const appSettings = {
+    databaseURL : "https://firstmobileapp-5f993-default-rtdb.firebaseio.com/"
+}
+
+const app = initializeApp(appSettings);
+const database = getDatabase(app);
+const shoppingListInDB = ref(database,"shoppingList");
+
 const inputField = document.getElementById("input-field");
 const addButton = document.getElementById("add-button");
+const shoppingListEl = document.getElementById("shopping-list");
 
 addButton.addEventListener("click", function(){
-    console.log(inputField.value);
+    let inputValue = inputField.value;
+
+    push(shoppingListInDB,inputValue);
+
+    clearInputField(inputField);
 });
+
+
+onValue(shoppingListInDB, function(snapshot){
+    if(snapshot.exists()){
+        let list = Object.entries(snapshot.val());
+
+        clearShoppingListEl();
+
+        for(let i = 0; i < list.length; i++){
+            let currentItem = list[i];
+            addHtmlData(currentItem);
+        }
+    } else{
+        shoppingListEl.innerHTML = "No items here... yet";
+    }
+})
+
+function clearInputField(field) {
+    field.value = "";
+}
+
+function clearShoppingListEl() {
+    shoppingListEl.innerHTML = "";
+}
+
+function addHtmlData(item) {
+    let itemID = item[0];
+    let itemValue = item[1];
+
+    let newEl = document.createElement("li");
+
+    newEl.textContent = itemValue;
+
+    newEl.addEventListener("dblclick", function(){
+        let exactLocationOfItemInDB = ref(database,`shoppingList/${itemID}`);
+        remove(exactLocationOfItemInDB);
+    })
+
+    shoppingListEl.append(newEl);
+}
